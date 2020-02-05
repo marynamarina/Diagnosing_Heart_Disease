57b8eaf216b61e4711a47a99cf92cead8f549152
marynamarina <57256884+marynamarina@users.noreply.github.com>
2020-02-05 14:21:52 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -1,28 +1,29 @@
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 import pickle
 
-dataset = pd.read_csv('hiring.csv')
+DF_URL = 'https://raw.githubusercontent.com/devrepublik/data-science-course/master/data/classification/HeartDisease_Clean.csv'
+stroke_df = pd.read_csv(DF_URL)
 
-x = dataset.iloc[:, :3]
-y = dataset.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(
+    stroke_df.drop('target', axis=1),
+    stroke_df['target'],
+    test_size=0.1,
+    random_state=42)
 
-#Splitting Training and Test Set
-#Since we have a very small dataset, we will train our model with all availabe data.
 
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
+regressor = RandomForestClassifier(
+    n_estimators=500,
+    criterion='entropy',
+    max_samples=0.5,
+    oob_score=True,
+    random_state=42)
 
 #Fitting model with trainig data
-regressor.fit(x, y)
+regressor.fit(X_train, y_train)
 
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
-
-'''
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 9, 6]]))
-'''
